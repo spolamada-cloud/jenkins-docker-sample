7a7798c3172e81fd3bd067b4e84464a377cf4203
saikrishna polamada <spolamada.cloud@gmail.com>
2024-10-21 10:15:14 +0530

    first commit

---
+++ src/main/java/com/docker/maven/example/SpringDockerMavenExampleApplication.java
@@ -0,0 +1,13 @@
+package com.docker.maven.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerMavenExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerMavenExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker/maven/example/SpringDockerMavenExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.maven.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerMavenExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
